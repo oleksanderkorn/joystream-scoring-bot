0282754ee457718e01e0426d2f7d930497926f5d
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2022-07-02 22:29:14 +0200

    Add cash command

--- src/bot-en.ts
+++ src/bot-en.ts
@@ -2,6 +2,8 @@ import TelegramBot, { SendMessageOptions } from "node-telegram-bot-api";
 import * as dotenv from "dotenv";
 import axios from "axios";
 import moment from "moment";
+import e from "cors";
+import { start } from "repl";
 interface ScoringPeriodData {
   balance: number;
 }
@@ -20,13 +22,27 @@ const chats: Set<number> = new Set();
 let intervalRef: NodeJS.Timeout | undefined;
 let lastKnownBalance = 0;
 
-moment.locale("ru");
+// moment.locale("ru");
 
 const loadBalance = async () => {
   const url = "https://status.joystream.org/cashout/balance";
   const { data } = await axios.get(url);
   return data;
 };
+
+const loadExchanges = async () => {
+  const url = "https://status.joystream.org/status";
+  const { data } = await axios.get(url);
+  return data.exchanges as Array<Exchange>;
+};
+
+interface Exchange {
+  sender: string;
+  amount: number;
+  amountUSD: number;
+  logTime: string;
+  status: string;
+}
 
 bot.on("message", async (msg: TelegramBot.Message) => {
   if (msg && msg.from) {
@@ -61,6 +77,46 @@ bot.on("message", async (msg: TelegramBot.Message) => {
       sendMessage(chatId, messageContent, msg);
     } else if (msg.text?.startsWith("/react")) {
       const messageContent = `Ебать у вас тут весело!`;
+      sendMessage(chatId, messageContent, msg, false);
+    } else if (msg.text?.startsWith("/cash")) {
+      const exchanges = await loadExchanges();
+
+      let myExchanges;
+
+      const msgArr = msg.text.split(" ");
+      if (msgArr.length > 1) {
+        myExchanges = exchanges
+          .filter((e) => e.sender === msgArr[1])
+          .sort((a, b) => (a.status > b.status ? -1 : 1));
+      } else {
+        const acc1 = "5CiRcZCWKDDo4nu1TNXnRVYWichHHmyU6x1nCUeDwjCNQRCw";
+        const acc2 = "5EiqT7y3DhzV4Sxuu2up2cAV6tow1nqUn1ntMAAJ3ynfMhHB";
+        myExchanges = exchanges
+          .filter((e) => e.sender === acc1 || e.sender === acc2)
+          .sort((a, b) => (a.status > b.status ? -1 : 1));
+      }
+
+      let messageContent = `My exchanges:\n`;
+      let totalAmount = 0;
+      let totalLeft = 0;
+      let totalPaid = 0;
+
+      for (let i = 0; i < myExchanges.length; i++) {
+        const e = myExchanges[i];
+        totalAmount += e.amountUSD;
+
+        const date = moment.parseZone(e.logTime).format("dddd DD MMM HH:mm");
+        if (e.status === "FINALIZED") {
+          totalPaid += e.amountUSD;
+        } else {
+          totalLeft += e.amountUSD;
+        }
+        let msg = `*Amount tJOY*: \`${e.amount}\`\n*Amount USD*: \`${e.amountUSD}\`\n*Sender*: \`${e.sender}\`\n*Date*: \`${date}\`\n*Status*: \`${e.status}\`\n\n`;
+        messageContent += msg;
+      }
+
+      messageContent = `*Total USD: ${totalAmount}*\n\n*Total Paid: ${totalPaid}*\n\n*Total Left: ${totalLeft}*\n\n${messageContent}\n\n`;
+
       sendMessage(chatId, messageContent, msg, false);
     }
   }
